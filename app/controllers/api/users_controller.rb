20e3d54742d8b76502ae801ae98acc0afa54be51
emilylichtenberg <erlichtenberg@gmail.com>
2022-02-26 14:10:48 -0500

    update email to password

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -12,6 +12,6 @@ class Api::UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:email, :password, :display_name)
+        params.require(:user).permit(:username, :password)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ApplicationRecord
-    validates :email, :display_name, :password_digest, presence: true
+    validates :username, :password_digest, presence: true
     validates :password, length: {minimum: 6, allow_nil: true}
-    validates :email, :session_token, uniqueness: true
+    validates :username, :session_token, uniqueness: true
 
     after_initialize :ensure_session_token
     attr_reader :password
 
-    def self.find_by_credentials(email, password)
-        @user = User.find_by(email: email)
+    def self.find_by_credentials(username, password)
+        @user = User.find_by(username: username)
         if @user && @user.is_password?(password)
             @user
         else
