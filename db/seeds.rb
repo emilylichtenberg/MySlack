2840645959b85e9f4a096fb60989b3e141136980
emilylichtenberg <erlichtenberg@gmail.com>
2022-03-03 22:53:40 -0500

    update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,11 +12,34 @@ user3 = User.create!(username: 'ethan', password: '123456')
 user3 = User.create!(username: 'kirby', password: '123456')
 
 workspace1 = Workspace.create!(name: 'App Academy', admin_id: 1)
-workspace2 = Workspace.create!(name: 'Other Company', admin_id: 1)
+workspace2 = Workspace.create!(name: 'Other Company', admin_id: 2)
 
 chat1 = Chat.create!(chat_type: 'channel', name: 'general', description: 'for general discussion', private: false, admin_id: 1, workspace_id: 1)
 chat2 = Chat.create!(chat_type: 'channel', name: 'test', description: 'test', private: true, admin_id: 1, workspace_id: 1)
+chat3 = Chat.create!(chat_type: 'channel', name: 'other', description: 'for general discussion', private: false, admin_id: 1, workspace_id: 2)
+chat4 = Chat.create!(chat_type: 'channel', name: 'other2', description: 'test', private: true, admin_id: 1, workspace_id: 2)
 # chat3 = Chat.create!(chat_type: 'DM', workspace_id: 1)
 # chat4 = Chat.create!(chat_type: 'GM', workspace_id: 1)
 
-message1 = Message.create!(body: 'first message')
+message1 = Message.create!(body: 'message1', chat_id: 1, sender_id: 1)
+message2 = Message.create!(body: 'message2', chat_id: 1, sender_id: 2)
+message3 = Message.create!(body: 'message3', chat_id: 2, sender_id: 1)
+message4 = Message.create!(body: 'message4', chat_id: 2, sender_id: 2)
+message5 = Message.create!(body: 'message5', chat_id: 3, sender_id: 1)
+message6 = Message.create!(body: 'message6', chat_id: 3, sender_id: 2)
+message7 = Message.create!(body: 'message7', chat_id: 4, sender_id: 1)
+message8 = Message.create!(body: 'message8', chat_id: 4, sender_id: 2)
+
+converation1 = Conversation.create!(chat_id: 1, user_id: 1)
+converation2 = Conversation.create!(chat_id: 1, user_id: 2)
+converation3 = Conversation.create!(chat_id: 2, user_id: 1)
+converation4 = Conversation.create!(chat_id: 2, user_id: 2)
+converation5 = Conversation.create!(chat_id: 3, user_id: 1)
+converation6 = Conversation.create!(chat_id: 3, user_id: 2)
+converation7 = Conversation.create!(chat_id: 4, user_id: 1)
+converation8 = Conversation.create!(chat_id: 4, user_id: 2)
+
+subscription1 = Subscription.create!(user_id: 1, workspace_id: 1);
+subscription2 = Subscription.create!(user_id: 1, workspace_id: 2);
+subscription3 = Subscription.create!(user_id: 2, workspace_id: 1);
+subscription4 = Subscription.create!(user_id: 2, workspace_id: 2);
--- frontend/actions/user_actions.js
+++ frontend/actions/user_actions.js
@@ -1,5 +1,5 @@
 import * as UserApiUtil from '../util/user_api_util'
-export const RECEIVE_USERS = 'RECEIVE_USERS';
+export const RECEIVE_USER = 'RECEIVE_USER';
 // export const RECEIVE_USER_WORKSPACES = 'RECEIVE_USER_WORKSPACES';
 // export const RECEIVE_USER_WORKSPACES = 'RECEIVE_USER_WORKSPACES';
 
