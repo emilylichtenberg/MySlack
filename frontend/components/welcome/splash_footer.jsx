ee91a198a97c609333caf3763d93faa055995c26
emilylichtenberg <erlichtenberg@gmail.com>
2022-04-04 10:47:44 -0400

    Update splash

--- frontend/components/welcome/splash_footer.jsx
+++ frontend/components/welcome/splash_footer.jsx
@@ -7,8 +7,8 @@ const SplashFooter = props => (
             <p>erlichtenberg@gmail.com</p>
         </div>
         <div className="splash-footer-right">
-            <button><a href="https://github.com/emilylichtenberg/mySlack" target="_blank"><i className='fab fa-github fa-3x'></i></a></button>
-            <button><a href="https://www.linkedin.com/in/emilylichtenberg/" target="_blank"><i className='fab fa-linkedin fa-3x'></i></a></button>
+            <button><a href="https://github.com/emilylichtenberg/mySlack" target="_blank"><i className='fab fa-github fa-2x'></i></a></button>
+            <button><a href="https://www.linkedin.com/in/emilylichtenberg/" target="_blank"><i className='fab fa-linkedin fa-2x'></i></a></button>
         </div>
     </div>  
 )
